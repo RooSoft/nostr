37350e3b02332f3aa207dd00613aea38ad6827d9
marc@roosoft.com <marc@roosoft.com>
2022-12-21 07:34:00 -0500

    added the k256 dependency

--- mix.exs
+++ mix.exs
@@ -21,8 +21,7 @@ defmodule Nostr.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:k256, git: "https://github.com/davidarmstronglewis/k256.git"}
     ]
   end
 end
