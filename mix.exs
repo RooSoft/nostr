5a1a9d6588748b9d1e014d208ef7ff768fc8cd12
marc@roosoft.com <marc@roosoft.com>
2022-12-22 14:20:44 -0500

    added ex_doc, will be ready to be deployed once usable and k256 is deployed itself

--- mix.exs
+++ mix.exs
@@ -1,13 +1,40 @@
 defmodule Nostr.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :nostr,
-      version: "0.1.0",
+      version: @version,
+      description: "Connect to the nostr network with Elixir",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Docs
+      name: "nostr",
+      source_url: "https://github.com/RooSoft/nostr",
+      homepage_url: "https://github.com/RooSoft/nostr",
+      package: package(),
+      docs: docs()
+    ]
+  end
+
+  def package do
+    [
+      maintainers: ["Marc Lacoursière"],
+      licenses: ["UNLICENCE"],
+      links: %{"GitHub" => "https://github.com/RooSoft/nostr"}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "nostr",
+      assets: "/guides/assets",
+      source_ref: @version,
+      source_url: "https://github.com/RooSoft/nostr"
     ]
   end
 
@@ -21,6 +48,7 @@ defmodule Nostr.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.29.1"},
       {:dialyxir, "~> 1.2"},
       {:websockex, "~> 0.4.3"},
       {:jason, "~> 1.4"},
