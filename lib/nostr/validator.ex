048cf137c221827201c5eb7eec3d86143b5a0f97
marc@roosoft.com <marc@roosoft.com>
2022-12-25 12:59:45 -0500

    can now validate signatures and ids

--- lib/nostr/validator.ex
+++ lib/nostr/validator.ex
@@ -1,15 +1,49 @@
 defmodule Nostr.Validator do
   alias K256.Schnorr
+  alias Nostr.Crypto
+
+  @text_event_kind 1
 
   def validate_note(%Nostr.Event.TextEvent{} = event) do
-    id = Binary.from_hex(event.id)
-    sig = Binary.from_hex(event.sig)
-    pubkey = Binary.from_hex(event.pubkey)
-
-    validate_signature(id, sig, pubkey)
+    with :ok <- validate_id(event),
+         :ok <- validate_signature(event) do
+      :ok
+    else
+      {:error, message} -> {:error, message}
+    end
   end
 
-  defp validate_signature(id, sig, pubkey) do
+  def validate_id(%Nostr.Event.TextEvent{id: id} = event) do
+    case id == create_id(event) do
+      true -> :ok
+      false -> {:error, "generated ID and the one in the event don't match"}
+    end
+  end
+
+  def validate_signature(%Nostr.Event.TextEvent{id: hex_id, sig: hex_sig, pubkey: hex_pubkey}) do
+    id = Binary.from_hex(hex_id)
+    sig = Binary.from_hex(hex_sig)
+    pubkey = Binary.from_hex(hex_pubkey)
+
     Schnorr.verify_message_digest(id, sig, pubkey)
+  end
+
+  defp create_id(%Nostr.Event.TextEvent{
+         pubkey: pubkey,
+         created_at: created_at,
+         tags: tags,
+         content: content
+       }) do
+    [
+      0,
+      pubkey,
+      created_at |> DateTime.to_unix(),
+      @text_event_kind,
+      tags,
+      content
+    ]
+    |> Jason.encode!()
+    |> Crypto.sha256()
+    |> Binary.to_hex()
   end
 end
--- test/nostr/validator_test.exs
+++ test/nostr/validator_test.exs
@@ -6,19 +6,36 @@ defmodule Nostr.ValidatorTest do
   setup_all do
     %{
       text_event: %Nostr.Event.TextEvent{
-        id: "c95f0243f8416c2261d306ff21d114e7b4bf336e4b844ccbc968ab2feea6dfa0",
-        content: "Been building on nostr all day... still do",
-        tags: [],
+        id: "c87a24fc125871887a632dd069b7a510bbf987fe7210f3e5bc67492ef461d87d",
+        content:
+          "Making sure the schnorr signature included with notes correspond to the public key",
+        tags: [
+          ["e", "4aa1f23601bb6c7275dca98bbfb6df593caeef0696f1ed260a0cb406d74d1fb0"],
+          ["e", "0500f45ca79ecf3a6e4dd6ecfd6a8c2ef2fedf8c590e60b22b98196a89ee2560"],
+          ["p", "98b62941fc20cfbb094e54b33593afa0090e43f263e92689a0b66b7e97cf39de"]
+        ],
         pubkey: "efc83f01c8fb309df2c8866b8c7924cc8b6f0580afdde1d6e16e2b6107c2862c",
         sig:
-          "2b694dfb688b74e1544db501fbf7b4cde78ce233235660f319b2c82e2d2a403cc000f8a015cae38f743585d1a81321d223a61bcc11d895277039e31c2b0028f2",
-        created_at: ~U[2022-12-23 01:56:18Z]
+          "1073eb38ba54982bf7a92139cecf23959d8cf6900ec44474bcecd9882b32f70afeadfda20620b1436f3ce9680a62261f126b92a5314fa27a4b0eab8f2447eabd",
+        created_at: ~U[2022-12-25 16:33:04Z]
       }
     }
   end
 
-  test "validate a note's signature", %{text_event: text_event} do
+  test "validate a note", %{text_event: text_event} do
     validation_result = Validator.validate_note(text_event)
+
+    assert :ok = validation_result
+  end
+
+  test "validate a note's id", %{text_event: text_event} do
+    validation_result = Validator.validate_id(text_event)
+
+    assert :ok = validation_result
+  end
+
+  test "validate a note's signature", %{text_event: text_event} do
+    validation_result = Validator.validate_signature(text_event)
 
     assert :ok = validation_result
   end
