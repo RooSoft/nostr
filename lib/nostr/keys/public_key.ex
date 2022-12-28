a9aeee409f6e5e7f5f8c0630fd48554b58efa19f
marc@roosoft.com <marc@roosoft.com>
2022-12-28 16:33:12 -0500

    clearer public key doctest

--- lib/nostr/keys/public_key.ex
+++ lib/nostr/keys/public_key.ex
@@ -3,8 +3,8 @@ defmodule Nostr.Keys.PublicKey do
   Issues the public key corresponding to a given private key
 
   ## Examples
-      iex> <<0xb6907368a945db7769b5eaecd73c3c175b77c64e1df3e9900acd66aeea7b53ab::256>>
-      ...> |> Nostr.Keys.PublicKey.from_private_key()
+      iex> private_key = <<0xb6907368a945db7769b5eaecd73c3c175b77c64e1df3e9900acd66aeea7b53ab::256>>
+      ...> Nostr.Keys.PublicKey.from_private_key(private_key)
       {:ok, <<0x6d72da1aa56f82aa9a7a8a7f2a94f46e2a80a6686dd60c182bbbc8ebef5811b1::256>>}
   """
   @spec from_private_key(<<_::256>>) :: {:ok, <<_::256>>} | {:error, binary()}
