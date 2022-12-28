8d219277f5b9c05f8292c4a046043b98525bb158
marc@roosoft.com <marc@roosoft.com>
2022-12-28 18:19:00 -0500

    documented the Nostr.Util.generate_random_id/1 function

--- lib/nostr/util.ex
+++ lib/nostr/util.ex
@@ -1,5 +1,14 @@
 defmodule Nostr.Util do
-  def generate_random_id() do
-    :crypto.strong_rand_bytes(16) |> Binary.to_hex()
+  @default_size 16
+
+  @doc """
+  Generates a random hex string composed of size*2 characters
+
+  ## Examples
+      iex> Nostr.Util.generate_random_id(16)
+  """
+  @spec generate_random_id(integer()) :: binary()
+  def generate_random_id(size \\ @default_size) do
+    :crypto.strong_rand_bytes(size) |> Binary.to_hex()
   end
 end
