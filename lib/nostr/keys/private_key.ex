debd87e7375bb1dc45b6ffd7e3f5b4444d744024
marc@roosoft.com <marc@roosoft.com>
2022-12-28 18:12:54 -0500

    added from_nsec/1 and to_nsec/1 to Nostr.Keys.PrivateKey

--- lib/nostr/keys/private_key.ex
+++ lib/nostr/keys/private_key.ex
@@ -9,4 +9,34 @@ defmodule Nostr.Keys.PrivateKey do
   def create do
     K256.Schnorr.generate_random_signing_key()
   end
+
+  @doc """
+  Converts a private key in the nsec format into a binary private key that can be used with this lib
+
+  ## Examples
+      iex> nsec = "nsec1d4ed5x49d7p24xn63flj4985dc4gpfngdhtqcxpth0ywhm6czxcs5l2exj"
+      ...> Nostr.Keys.PrivateKey.from_nsec(nsec)
+      {:ok, <<0x6d72da1aa56f82aa9a7a8a7f2a94f46e2a80a6686dd60c182bbbc8ebef5811b1::256>>}
+  """
+  @spec from_nsec(binary()) :: {:ok, binary()} | {:error, binary()}
+  def from_nsec("nsec" <> _ = bech32_private_key) do
+    case Bech32.decode(bech32_private_key) do
+      {:ok, "nsec", private_key} -> {:ok, private_key}
+      {:ok, _, _} -> {:error, "malformed bech32 private key"}
+      {:error, message} -> {:error, message}
+    end
+  end
+
+  @doc """
+  Encodes a private key into the nsec format
+
+  ## Examples
+      iex> private_key = <<0x6d72da1aa56f82aa9a7a8a7f2a94f46e2a80a6686dd60c182bbbc8ebef5811b1::256>>
+      ...> Nostr.Keys.PrivateKey.to_nsec(private_key)
+      "nsec1d4ed5x49d7p24xn63flj4985dc4gpfngdhtqcxpth0ywhm6czxcs5l2exj"
+  """
+  @spec to_nsec(<<_::256>>) :: binary()
+  def to_nsec(<<_::256>> = private_key) do
+    Bech32.encode("nsec", private_key)
+  end
 end
