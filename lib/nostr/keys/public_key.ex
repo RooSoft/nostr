94c1c18cd13909efef3b98c549aac170c004e287
marc@roosoft.com <marc@roosoft.com>
2022-12-28 16:38:54 -0500

    added the Nostr.Keys.PublicKey.from_npub function

--- lib/nostr/keys/public_key.ex
+++ lib/nostr/keys/public_key.ex
@@ -11,4 +11,21 @@ defmodule Nostr.Keys.PublicKey do
   def from_private_key(private_key) do
     K256.Schnorr.verifying_key_from_signing_key(private_key)
   end
+
+  @doc """
+  Converts a bech32 string into a binary public key that can be used with this lib
+
+  ## Examples
+      iex> npub = "npub1d4ed5x49d7p24xn63flj4985dc4gpfngdhtqcxpth0ywhm6czxcscfpcq8"
+      ...> Nostr.Keys.PublicKey.from_npub(npub)
+      {:ok, <<0x6d72da1aa56f82aa9a7a8a7f2a94f46e2a80a6686dd60c182bbbc8ebef5811b1::256>>}
+  """
+  @spec from_npub(binary()) :: {:ok, binary()} | {:error, binary()}
+  def from_npub("npub" <> _ = bech32_pubkey) do
+    case Bech32.decode(bech32_pubkey) do
+      {:ok, "npub", pubkey} -> {:ok, pubkey}
+      {:ok, _, _} -> {:error, "malformed bech32 public key"}
+      {:error, message} -> {:error, message}
+    end
+  end
 end
