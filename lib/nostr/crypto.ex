a01ec890a1903d392c0e341f477cc4ccbb752d68
marc@roosoft.com <marc@roosoft.com>
2022-12-29 13:21:34 -0500

    cleaner test in Nostr.Crypto

--- lib/nostr/crypto.ex
+++ lib/nostr/crypto.ex
@@ -4,7 +4,7 @@ defmodule Nostr.Crypto do
   ## Examples
       iex> "6c7ab2f961a1bc3f13cdc08dc41c3f439adebd549a8ef1c089e81a5907376107"
       ...> |> Nostr.Crypto.sha256()
-      <<171, 106, 143, 29, 158, 43, 3, 51, 223, 248, 227, 112, 237, 111, 223, 226, 11, 46, 128, 8, 224, 69, 239, 179, 251, 50, 152, 194, 47, 117, 105, 218>>
+      <<0xab6a8f1d9e2b0333dff8e370ed6fdfe20b2e8008e045efb3fb3298c22f7569da::256>>
   """
   @spec sha256(String.t()) :: <<_::256>>
   def sha256(bin) when is_bitstring(bin) do
