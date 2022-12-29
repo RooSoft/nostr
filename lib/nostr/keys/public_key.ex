c3ebaee8736a367388173de72aee5fd49229bc7b
marc@roosoft.com <marc@roosoft.com>
2022-12-28 21:29:00 -0500

    added Nostr.Keys.PublicKey.from_private_key!/1

--- lib/nostr/keys/public_key.ex
+++ lib/nostr/keys/public_key.ex
@@ -13,6 +13,22 @@ defmodule Nostr.Keys.PublicKey do
   end
 
   @doc """
+  Issues the public key corresponding to a given private key
+
+  ## Examples
+      iex> private_key = <<0xb6907368a945db7769b5eaecd73c3c175b77c64e1df3e9900acd66aeea7b53ab::256>>
+      ...> Nostr.Keys.PublicKey.from_private_key!(private_key)
+      <<0x6d72da1aa56f82aa9a7a8a7f2a94f46e2a80a6686dd60c182bbbc8ebef5811b1::256>>
+  """
+  @spec from_private_key!(<<_::256>>) :: <<_::256>>
+  def from_private_key!(private_key) do
+    case from_private_key(private_key) do
+      {:ok, public_key} -> public_key
+      {:error, message} -> raise message
+    end
+  end
+
+  @doc """
   Converts a public key in the npub format into a binary public key that can be used with this lib
 
   ## Examples
@@ -37,7 +53,7 @@ defmodule Nostr.Keys.PublicKey do
       ...> Nostr.Keys.PublicKey.from_npub!(npub)
       <<0x6d72da1aa56f82aa9a7a8a7f2a94f46e2a80a6686dd60c182bbbc8ebef5811b1::256>>
   """
-  @spec from_npub!(binary()) :: {:ok, binary()} | {:error, binary()}
+  @spec from_npub!(binary()) :: <<_::256>>
   def from_npub!("npub" <> _ = bech32_pubkey) do
     case from_npub(bech32_pubkey) do
       {:ok, pubkey} -> pubkey
