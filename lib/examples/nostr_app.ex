2f7d19970a5e7720e6883882fc0645a23000cd6e
marc@roosoft.com <marc@roosoft.com>
2022-12-31 09:03:09 -0500

    separated the nostr_app with its gen_server

--- lib/examples/nostr_app.ex
+++ lib/examples/nostr_app.ex
@@ -1,96 +1,17 @@
 defmodule NostrApp do
-  use GenServer
-
-  alias Nostr.Client
-  alias Nostr.Event.Types.{EncryptedDirectMessageEvent, TextEvent}
+  alias NostrApp.Server
 
   def start_link(relay, <<_::256>> = private_key) do
     args = %{relay: relay, private_key: private_key}
 
-    GenServer.start_link(__MODULE__, args, name: __MODULE__)
-  end
-
-  @impl true
-  def init(%{relay: relay, private_key: private_key} = args) do
-    with {:ok, public_key} <- K256.Schnorr.verifying_key_from_signing_key(private_key),
-         {:ok, nostr_client_pid} <- Nostr.Client.start_link(relay) do
-      {
-        :ok,
-        args
-        |> Map.put(:nostr_client_pid, nostr_client_pid)
-        |> Map.put(:public_key, public_key)
-      }
-    else
-      {:error, message} ->
-        {:stop, message}
-    end
+    GenServer.start_link(Server, args, name: Server)
   end
 
   def send(note) do
-    GenServer.cast(__MODULE__, {:send, note})
+    GenServer.cast(Server, {:send, note})
   end
 
   def contacts(pubkey) do
-    GenServer.cast(__MODULE__, {:contacts, pubkey})
-  end
-
-  @impl true
-  def handle_cast(
-        {:send, note},
-        %{nostr_client_pid: nostr_client_pid, private_key: private_key} = socket
-      ) do
-    Client.send_note(nostr_client_pid, note, private_key)
-
-    {:noreply, socket}
-  end
-
-  @impl true
-  def handle_cast(
-        {:contacts, pubkey},
-        %{nostr_client_pid: nostr_client_pid} = socket
-      ) do
-    Client.get_contacts(nostr_client_pid, pubkey)
-
-    {:noreply, socket}
-  end
-
-  @impl true
-  def handle_info(
-        :connected,
-        %{nostr_client_pid: nostr_client_pid, public_key: public_key} = socket
-      ) do
-    _request_id =
-      Client.subscribe_author(
-        nostr_client_pid,
-        public_key,
-        10
-      )
-
-    {:noreply, socket}
-  end
-
-  @impl true
-  def handle_info({request_id, %TextEvent{event: event}}, socket) do
-    IO.puts("TEXT EVENT")
-    IO.inspect(event, label: "#{request_id}")
-    IO.inspect(Nostr.Event.Validator.validate_event(event))
-
-    {:noreply, socket}
-  end
-
-  @impl true
-  def handle_info({request_id, %EncryptedDirectMessageEvent{event: event}}, socket) do
-    IO.puts("ENCRYPTED DM EVENT")
-    IO.inspect(event, label: "#{request_id}")
-    IO.inspect(Nostr.Event.Validator.validate_event(event))
-
-    {:noreply, socket}
-  end
-
-  @impl true
-  def handle_info({request_id, event}, socket) do
-    IO.inspect(event, label: "#{request_id}")
-
-    {:noreply, socket}
+    GenServer.cast(Server, {:contacts, pubkey})
   end
 end
---
+++ lib/examples/nostr_app/server.ex
@@ -0,0 +1,83 @@
+defmodule NostrApp.Server do
+  use GenServer
+
+  alias Nostr.Client
+  alias Nostr.Event.Types.{EncryptedDirectMessageEvent, TextEvent}
+
+
+  @impl true
+  def init(%{relay: relay, private_key: private_key} = args) do
+    with {:ok, public_key} <- K256.Schnorr.verifying_key_from_signing_key(private_key),
+         {:ok, nostr_client_pid} <- Nostr.Client.start_link(relay) do
+      {
+        :ok,
+        args
+        |> Map.put(:nostr_client_pid, nostr_client_pid)
+        |> Map.put(:public_key, public_key)
+      }
+    else
+      {:error, message} ->
+        {:stop, message}
+    end
+  end
+
+  @impl true
+  def handle_cast(
+        {:send, note},
+        %{nostr_client_pid: nostr_client_pid, private_key: private_key} = socket
+      ) do
+    Client.send_note(nostr_client_pid, note, private_key)
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_cast(
+        {:contacts, pubkey},
+        %{nostr_client_pid: nostr_client_pid} = socket
+      ) do
+    Client.get_contacts(nostr_client_pid, pubkey)
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info(
+        :connected,
+        %{nostr_client_pid: nostr_client_pid, public_key: public_key} = socket
+      ) do
+    _request_id =
+      Client.subscribe_author(
+        nostr_client_pid,
+        public_key,
+        10
+      )
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info({request_id, %TextEvent{event: event}}, socket) do
+    IO.puts("TEXT EVENT")
+    IO.inspect(event, label: "#{request_id}")
+    IO.inspect(Nostr.Event.Validator.validate_event(event))
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info({request_id, %EncryptedDirectMessageEvent{event: event}}, socket) do
+    IO.puts("ENCRYPTED DM EVENT")
+    IO.inspect(event, label: "#{request_id}")
+    IO.inspect(Nostr.Event.Validator.validate_event(event))
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info({request_id, event}, socket) do
+    IO.inspect(event, label: "#{request_id}")
+
+    {:noreply, socket}
+  end
+end
