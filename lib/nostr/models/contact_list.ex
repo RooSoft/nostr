f6e6e54cc740c10a945cf632f867230ee2710c5d
marc@roosoft.com <marc@roosoft.com>
2023-01-12 16:51:40 -0500

    added the add function to ContactList

--- lib/nostr/models/contact_list.ex
+++ lib/nostr/models/contact_list.ex
@@ -1,3 +1,11 @@
 defmodule Nostr.Models.ContactList do
   defstruct [:id, :pubkey, :created_at, :contacts]
+
+  alias Nostr.Models.ContactList
+
+  def add(%ContactList{contacts: contacts} = contact_list, pubkey) do
+    new_contacts = [pubkey | contacts]
+
+    %{contact_list | contacts: new_contacts}
+  end
 end
