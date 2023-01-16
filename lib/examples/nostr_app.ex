04b9dae9d3bd8dda2c54a6fd3e97a34902b4a871
marc@roosoft.com <marc@roosoft.com>
2023-01-16 12:59:48 -0500

    reordering and NIP-labeling of functions in the example app

--- lib/examples/nostr_app.ex
+++ lib/examples/nostr_app.ex
@@ -9,58 +9,72 @@ defmodule NostrApp do
     GenServer.start_link(Server, args, name: Server)
   end
 
-  def send_note(note) do
-    GenServer.cast(Server, {:send_note, note})
-  end
-
-  def react(note_id) do
-    GenServer.cast(Server, {:react, note_id})
-  end
-
+  ### NIP-01
   def profile(pubkey \\ nil) do
     GenServer.cast(Server, {:profile, pubkey})
   end
 
+  ### NIP-01
   def update_profile(%Profile{} = profile) do
     GenServer.cast(Server, {:update_profile, profile})
   end
 
-  def contacts(pubkey) do
-    GenServer.cast(Server, {:contacts, pubkey})
-  end
-
-  def follow(pubkey) do
-    GenServer.cast(Server, {:follow, pubkey})
-  end
-
-  def unfollow(pubkey) do
-    GenServer.cast(Server, {:unfollow, pubkey})
-  end
-
+  ### NIP-01
   def note(note_id) do
     GenServer.cast(Server, {:note, note_id})
   end
 
+  ### NIP-01
   def notes(pubkey) do
     GenServer.cast(Server, {:notes, pubkey})
   end
 
+  ### NIP-01
+  def send_note(note) do
+    GenServer.cast(Server, {:send_note, note})
+  end
+
+  ### NIP-02
+  def contacts(pubkey) do
+    GenServer.cast(Server, {:contacts, pubkey})
+  end
+
+  ### NIP-02
+  def follow(pubkey) do
+    GenServer.cast(Server, {:follow, pubkey})
+  end
+
+  ### NIP-02
+  def unfollow(pubkey) do
+    GenServer.cast(Server, {:unfollow, pubkey})
+  end
+
+  ### NIP-09
   def deletions(pubkeys) when is_list(pubkeys) do
     GenServer.cast(Server, {:deletions, pubkeys})
   end
 
+  ### NIP-18
   def reposts() do
     GenServer.cast(Server, {:reposts})
   end
 
+  ### NIP-18
   def reposts(pubkeys) when is_list(pubkeys) do
     GenServer.cast(Server, {:reposts, pubkeys})
   end
 
+  ### NIP-25
+  def react(note_id) do
+    GenServer.cast(Server, {:react, note_id})
+  end
+
+  ### NIP-25
   def reactions(pubkeys) when is_list(pubkeys) do
     GenServer.cast(Server, {:reactions, pubkeys})
   end
 
+  ### Combination of multiple NIPs
   def timeline(pubkey) do
     GenServer.cast(Server, {:timeline, pubkey})
   end
