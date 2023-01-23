f0449cc28bdf055d745ea895bdd084ab6c73db2d
marc@roosoft.com <marc@roosoft.com>
2023-01-23 12:02:20 -0500

    added typespecs to the nostr_app subscribe module

--- lib/examples/nostr_app/subscribe.ex
+++ lib/examples/nostr_app/subscribe.ex
@@ -6,7 +6,10 @@ defmodule NostrApp.Subscribe do
   require Logger
 
   alias Nostr.Client
+  alias Nostr.Keys.{PublicKey, PrivateKey}
+  alias Nostr.Models.Note
 
+  @spec to_profile(PublicKey.id()) :: :ok
   def to_profile(public_key) do
     case Client.subscribe_profile(public_key) do
       {:ok, _} -> Logger.info("Subscribed to #{inspect(public_key)}'s profile")
@@ -14,6 +17,7 @@ defmodule NostrApp.Subscribe do
     end
   end
 
+  @spec to_contacts(PublicKey.id()) :: :ok
   def to_contacts(public_key) do
     case Client.subscribe_contacts(public_key) do
       {:ok, _} -> Logger.info("Subscribed to #{inspect(public_key)}'s contact list")
@@ -21,6 +25,7 @@ defmodule NostrApp.Subscribe do
     end
   end
 
+  @spec to_note(Note.id()) :: :ok
   def to_note(note_id) do
     case Client.subscribe_note(note_id) do
       {:ok, _} -> Logger.info("Subscribed to this note: #{inspect(note_id)}")
@@ -28,6 +33,7 @@ defmodule NostrApp.Subscribe do
     end
   end
 
+  @spec to_notes(list(PublicKey.id())) :: :ok
   def to_notes(public_keys) do
     case Client.subscribe_notes(public_keys) do
       {:ok, _} -> Logger.info("Subscribed to notes from: #{inspect(public_keys)}")
@@ -35,6 +41,7 @@ defmodule NostrApp.Subscribe do
     end
   end
 
+  @spec to_encrypted_direct_messages(PrivateKey.id()) :: :ok
   def to_encrypted_direct_messages(private_key) do
     case Client.encrypted_direct_messages(private_key) do
       {:ok, _} -> Logger.info("Subscribed to #{inspect(private_key)}'s encrypted messages")
@@ -42,6 +49,7 @@ defmodule NostrApp.Subscribe do
     end
   end
 
+  @spec to_reactions(list(PublicKey.id())) :: :ok
   def to_reactions(public_keys) do
     case Client.subscribe_reactions(public_keys) do
       {:ok, _} -> Logger.info("Subscribed to #{inspect(public_keys)}'s reactions")
@@ -49,6 +57,7 @@ defmodule NostrApp.Subscribe do
     end
   end
 
+  @spec to_deletions(list(PublicKey.id())) :: :ok
   def to_deletions(public_keys) do
     case Client.subscribe_deletions(public_keys) do
       {:ok, _} -> Logger.info("Subscribed to #{inspect(public_keys)}'s deletions")
@@ -56,6 +65,7 @@ defmodule NostrApp.Subscribe do
     end
   end
 
+  @spec to_reposts(list(PublicKey.id())) :: :ok
   def to_reposts(public_keys) do
     case Client.subscribe_reposts(public_keys) do
       {:ok, _} -> Logger.info("Subscribed to #{inspect(public_keys)}'s reposts")
@@ -63,6 +73,7 @@ defmodule NostrApp.Subscribe do
     end
   end
 
+  @spec to_timeline(PublicKey.id()) :: :ok
   def to_timeline(public_key) do
     case Client.subscribe_timeline(public_key) do
       {:ok, _} -> Logger.info("Subscribed to #{inspect(public_key)}'s timeline")
