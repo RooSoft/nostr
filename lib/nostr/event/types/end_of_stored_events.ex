37ae62f53b8dadbe4d3ec9c811581e997fee8951
marc@roosoft.com <marc@roosoft.com>
2023-02-14 13:15:08 -0500

    removed EndOfStoredEvents

--- lib/nostr/event/types/end_of_stored_events.ex
+++
@@ -1,7 +0,0 @@
-defmodule Nostr.Event.Types.EndOfStoredEvents do
-  @moduledoc """
-  Representing an empty event that signals the end of a list of recorded event
-  """
-
-  defstruct [:relay_url, :subscription_id]
-end
